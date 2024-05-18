6f8c822f4a65fea8ee8c527631cfeed93eabcb22
ACC User <accuser@ACCs-MacBook-Pro.local>
2024-05-18 09:30:05 +0530

    Test husky

--- src/utils/formatFuturesData.js
+++ src/utils/formatFuturesData.js
@@ -1,28 +1,30 @@
-export const callPutSummationTable = (data) => {
-  console.log("data", data);
-  const { range } = data;
-  const start = range.start;
-  let currVal = start;
-  const tableHeaders = ["index", `${currVal}Put`];
-  while (currVal !== range.end) {
-    currVal += 50;
-    tableHeaders.push(`${currVal}Put`);
-  }
-  const tableBody = new Array(25).fill({
-    call: "22150Call",
-    putCallSum1: "371.8",
-    putCallSum2: "371.8",
-    putCallSum3: "371.8",
-    putCallSum4: "371.8",
-    putCallSum5: "371.8",
-    putCallSum6: "371.8",
-    putCallSum7: "371.8",
-    putCallSum8: "371.8",
-    putCallSum9: "371.8",
-    putCallSum10: "371.8",
-  });
-  return { tableHeaders, tableBody };
+const callPutSummationTable = (data) => {
+	console.log('data', data);
+	const { range } = data;
+	const { start } = range;
+	let currVal = start;
+	const tableHeaders = ['index', `${currVal}Put`];
+	while (currVal !== range.end) {
+		currVal += 50;
+		tableHeaders.push(`${currVal}Put`);
+	}
+	const tableBody = new Array(25).fill({
+		call: '22150Call',
+		putCallSum1: '371.8',
+		putCallSum2: '371.8',
+		putCallSum3: '371.8',
+		putCallSum4: '371.8',
+		putCallSum5: '371.8',
+		putCallSum6: '371.8',
+		putCallSum7: '371.8',
+		putCallSum8: '371.8',
+		putCallSum9: '371.8',
+		putCallSum10: '371.8',
+	});
+	return { tableHeaders, tableBody };
 };
 
 // const result = callPutSummationTable({ range: { start: 20000, end: 22000 } });
 // console.log("result", result);
+
+export default callPutSummationTable;
