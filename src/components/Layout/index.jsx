110b656456a2f779756df156890c2ea14bd4ccdc
ACC User <accuser@ACCs-MacBook-Pro.local>
2024-05-20 03:04:55 +0530

    Modified: BuySellOptions Flex Gap

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,7 +3,8 @@ import ExpiryLayout from '../expiryLayout';
 
 function Layout() {
 	return (
-		<div className="h-[100vh] grid grid-rows-10 gap-4">
+		<div className="h-[100vh] grid grid-cols-10">
+			{/* <div className="h-[100vh] grid grid-rows-10 gap-4"> */}
 			<ExpiryLayout />
 			<ExpiryLayout />
 		</div>
